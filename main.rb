8254cfb79c785acf601cb3bd6675504412f24388
takeda29 <2yc5oa3un6@gmail.com>
2017-09-28 15:52:20 +0900

    アイテム機能を追加

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 # coding: utf-8
 require 'dxruby'
 
-require_relative 'arm'
 require_relative 'player'
 require_relative 'enemy'
+require_relative 'arm'
+require_relative 'item'
 
 font = Font.new(32)
 
@@ -19,40 +20,81 @@ enemy_img.setColorKey([0, 0, 0])
 bu_img = Image.load("ring.png")
 bu_img.setColorKey([0, 0, 0])
 
+usp_img = Image.load("speedup.png")
+usp_img.setColorKey([0, 0, 0])
+
+dsp_img = Image.load("speeddown.png")
+dsp_img.setColorKey([0, 0, 0])
+
+usc_img = Image.load("scoreup.png")
+usc_img.setColorKey([0, 0, 0])
+
+dsc_img = Image.load("scoredown.png")
+dsc_img.setColorKey([0, 0, 0])
+
 player = Player.new(400, 500, player_img)
 player.init
 
 enemies = []
 bullets = []
+uspeed_items = []
+dspeed_items = []
+score_items = []
+
 Arm.init
 
-count = 25
-at_count = false
+e_count = 25
+usp_count = 100
+dsp_count = 100
 
+# スタート画面
 Window.loop do
   Window.draw_font(200, 200, "GAMESTART:Push space", font)
   break if Input.key_push?(K_SPACE)
 end
 
+#
 Window.loop do
   break if Input.key_push?(K_ESCAPE)
 
-  count -= 1
-  if count == 0
-    count = 25
+  #　敵のインスタンス
+  e_count -= 1
+  if e_count == 0
+    e_count = 25
     enemies << Enemy.new(rand(800), rand(200), enemy_img)
+  end
+
+  #　加速アイテムのインスタンス
+  usp_count -= 1
+  if usp_count == 0
+    usp_count = 100
+    uspeed_items << Item.new(rand(800), rand(200), usp_img)
+  end
+
+  #　減速アイテムのインスタンス
+  dsp_count -= 1
+  if dsp_count == 0
+    dsp_count = 100
+    dspeed_items << Item.new(rand(800), rand(200), dsp_img)
+  end
+
+  # 弾のインスタンス
+  if Input.key_push?(K_SPACE)
+    bullets << Arm.new(player.x, player.y, bu_img)
+    Arm.set
   end
 
   Sprite.update(enemies)
   Sprite.draw(enemies)
 
+  Sprite.update(uspeed_items)
+  Sprite.draw(uspeed_items)
+
+  Sprite.update(dspeed_items)
+  Sprite.draw(dspeed_items)
+
   player.update
   player.draw
-
-  if Input.key_push?(K_SPACE)
-    bullets << Arm.new(player.x, player.y, bu_img)
-    Arm.set
-  end
 
   if Arm.ref
     Sprite.update(bullets)
@@ -61,10 +103,14 @@ Window.loop do
 
   # 当たり判定
   Sprite.check(player, enemies)
+  Sprite.check(player, uspeed_items, :upspeed)
+  Sprite.check(player, dspeed_items, :downspeed)
   if Arm.ref
     Sprite.check(bullets, enemies)
+    Sprite.check(bullets, dspeed_items)
   end
 
+  # ステータス表示
   score = Arm.score
   life = player.showlife
   Window.draw_font(0, 0, "Score:#{score}", font)
@@ -74,6 +120,7 @@ Window.loop do
   end
 end
 
+# 終了画面
 Window.loop do
   break if Input.keyPush?(K_ESCAPE)
 
--- player.rb
+++ player.rb
@@ -4,10 +4,11 @@ class Player < Sprite
   def init
     @life = 3
     @k_flag = false
+    @speed = 1.0
   end
 
   def update
-    self.x += Input.x * 1.5
+    self.x += Input.x * @speed
     # Playerは画面内にとどまる
     if self.x >= Window.width - self.image.width
       self.x = Window.width - self.image.width
@@ -16,7 +17,7 @@ class Player < Sprite
     end
 
     # Playerは画面内にとどまる
-    self.y += Input.y * 1.5
+    self.y += Input.y * @speed
     if self.y >= Window.height - self.image.height
       self.y = Window.height - self.image.height
     elsif self.y <= 0
@@ -26,9 +27,24 @@ class Player < Sprite
 
   # 他のオブジェクトと衝突した際に呼ばれるメソッド
   def shot(obj)
-    @life -= 1
+      @life -= 1
+      @speed = 1.0
   end
 
+  # 加速アイテムを取得時に呼ばれるメソッド
+  def upspeed
+    @speed += 0.5
+  end
+
+  # 減速アイテムを取得時に呼ばれるメソッド
+  def downspeed
+    @speed -= 0.5
+    if @speed <= 0
+      @speed = 1.0
+    end
+  end
+
+  # ライフを返すメソッド
   def showlife
     @life
   end
