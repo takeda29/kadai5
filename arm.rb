182bf6c02d6f03ebee60d04e146a5a29bff6bd3c
takeda29 <2yc5oa3un6@gmail.com>
2017-09-28 16:45:58 +0900

    修正

--- arm.rb
+++ arm.rb
@@ -8,14 +8,6 @@ class Arm < Sprite
       end
   end
 
-  def flag_set
-    @a_flag = true
-  end
-
-  def flag_check
-    @a_flag
-  end
-
   # アイテムと衝突した際に呼ばれるメソッド
   def eneen(obj)
     $score += 1
